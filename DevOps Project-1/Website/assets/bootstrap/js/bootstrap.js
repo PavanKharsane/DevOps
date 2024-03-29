64d5d32246027a1d287ee4694d0cd3d0bea4e55d
Pavan kharsane <118529808+PavanKharsane@users.noreply.github.com>
2024-03-29 13:45:24 +0530

    Update bootstrap.js

--- DevOps Project-1/Website/assets/bootstrap/js/bootstrap.js
+++ DevOps Project-1/Website/assets/bootstrap/js/bootstrap.js
@@ -73,10 +73,7 @@ if (typeof jQuery === 'undefined') {
 /* ========================================================================
  * Bootstrap: alert.js v3.3.2
  * http://getbootstrap.com/javascript/#alerts
- * ========================================================================
- * Copyright 2011-2015 Twitter, Inc.
- * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
- * ======================================================================== */
+ * ========================================================================*/
 
 
 +function ($) {
